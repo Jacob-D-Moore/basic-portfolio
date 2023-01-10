6d1c2871ef4d132fe8be8368003e3285b53c3d5a
Jacob-D-Moore <moore.jacob7@gmail.com>
2023-01-10 08:05:32 -0500

    fixed porfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,28 @@
+/** @format */
+
 export default [
-  {
-    title: "Portfolio",
-    imgUrl: "/assets/portfolio.jpg",
-    stack: ["React", "Tailwind", "Vite"],
-    link: "https://jmoore.info",
-  },
-  {
-    title: "Tip Calculator",
-    imgUrl: "/assets/tip_calculator.jpg",
-    stack: ["HTML", "CSS", "JavaScript"],
-    link: "https://tip-calc-js.netlify.app/",
-  },
-  {
-    title: "Guess My Number",
-    imgUrl: "/assets/guess-my-number.jpg",
-    stack: ["HTML", "CSS", "JavaScript"],
-    link: "https://guess-my-numnber.netlify.app/",
-  },
-  {
-    title: "First To 100 Game",
-    imgUrl: "/assets/dice-game.jpg",
-    stack: ["HTML", "CSS", "JavaScript"],
-    link: "https://the-first-to-100-game.netlify.app/",
-  },
+	// {
+	//   title: "Portfolio",
+	//   imgUrl: "/assets/portfolio.jpg",
+	//   stack: ["React", "Tailwind", "Vite"],
+	//   link: "https://jmoore.info",
+	// },
+	{
+		title: "Tip Calculator",
+		imgUrl: "/assets/tip_calculator.jpg",
+		stack: ["HTML", "CSS", "JavaScript"],
+		link: "https://tip-calc-js.netlify.app/",
+	},
+	{
+		title: "Guess My Number",
+		imgUrl: "/assets/guess-my-number.jpg",
+		stack: ["HTML", "CSS", "JavaScript"],
+		link: "https://guess-my-numnber.netlify.app/",
+	},
+	{
+		title: "First To 100 Game",
+		imgUrl: "/assets/dice-game.jpg",
+		stack: ["HTML", "CSS", "JavaScript"],
+		link: "https://the-first-to-100-game.netlify.app/",
+	},
 ];
